1523d112cface75b84c4cd540935d0fb4451ebd6
SIGEGV <officialyashdobriyal@gmail.com>
2025-04-11 00:51:45 +0530

    new branch commit

--- src/models/comments.model.js
+++ src/models/comments.model.js
@@ -1,4 +1,4 @@
 import mongoose, { Schema } from "mongoose";
 import { SCHEMA_NAMES } from "./models.constansts";
-const COMMENTS_SCHEMA = new mongoose({}, { timestamps: true });
+const COMMENTS_SCHEMA = new Schema({}, { timestamps: true });
 export const COMMENTS = mongoose.model(SCHEMA_NAMES.COMMENTS, COMMENTS_SCHEMA);
